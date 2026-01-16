72e53552b15c1b43a2c2660482636a613970d96e
Mohammed Yahya <yahya94812@gmail.com>
2026-01-16 19:47:06 +0530

    undo compatiblity with github pages

--- routing-in-react/react-real-routing/vite.config.ts
+++ routing-in-react/react-real-routing/vite.config.ts
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set base to your GitHub repo name for GitHub Pages deployment
-  // Replace 'react-real-routing' with your actual repo name
-  base: '/react-real-routing/',
 })
